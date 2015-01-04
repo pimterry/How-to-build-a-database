be5865bc37830656979cfc476265b7b722d02296
pimterry <pimterry@gmail.com>
2015-01-04 23:21:23 +0000

    Move from a dict to a B+ tree

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-import os, json, cherrypy
+import os, json, logging, cherrypy
+from blist import sorteddict
 from flask import Flask, abort, request, make_response
 
 def build_app():
   app = Flask(__name__)
   app.debug = True
 
-  data_dict = { }
+  data = sorteddict()
+  keys = data.keys()
+  values = data.values()
 
   @app.route("/reset", methods=["POST"])
   def reset():
-      data_dict.clear()
+      data.clear()
       return make_response("", 200)
 
   @app.route("/<int:item_id>", methods=["GET"])
   def get_item(item_id):
     try:
-      return "%s" % (data_dict[item_id],)
+      return "%s" % (data[item_id],)
     except KeyError:
       raise abort(404)
 
   @app.route("/range")
   def get_range():
     start, end = int(request.args.get('start')), int(request.args.get('end'))
-    keys = range(start, end + 1)
 
-    return json.dumps([data_dict[k] for k in keys if k in data_dict])
+    start_index, end_index = keys.bisect_left(start), keys.bisect_right(end)
+
+    relevant_values = values[start_index:end_index]
+
+    return json.dumps(relevant_values)
 
   @app.route("/<int:item_id>", methods=["POST"])
   def post_item(item_id):
     value = int(request.data)
-    data_dict[item_id] = value
+    data[item_id] = value
     return make_response(str(value), 201)
 
   return app
@@ -39,9 +45,9 @@ def run_server(app):
 
     cherrypy.config.update({
         'server.socket_port': int(os.environ.get('PORT', '8080')),
-        'server.socket_host': '0.0.0.0',
-        'environment': 'production'
+        'server.socket_host': '0.0.0.0'
     })
+    cherrypy.log.error_log.setLevel(logging.WARNING)
 
     cherrypy.engine.start()
     cherrypy.engine.block()
--- range-test.py
+++ range-test.py
@@ -1,4 +1,5 @@
-import requests, random
+import requests, random, unittest
+from timeout_decorator import timeout
 from dbtestcase import DbTestCase, DB_ROOT
 
 class Item:
@@ -14,19 +15,37 @@ class Range:
 class RangeTests(DbTestCase):
 
   def test_can_get_two_value_range(self):
-    requests.post(Item(0).url, "1")
-    requests.post(Item(1).url, "2")
+    requests.post(Item(0).url, "0")
+    requests.post(Item(1).url, "1")
 
     read = requests.get(Range(0, 1).url)
 
-    self.assertReturns(read, [1, 2])
+    self.assertReturns(read, [0, 1])
 
   def test_can_get_single_value_range(self):
-    requests.post(Item(10).url, "0")
+    requests.post(Item(10).url, "123")
 
     read = requests.get(Range(10, 10).url)
 
-    self.assertReturns(read, [0])
+    self.assertReturns(read, [123])
+
+  def test_doesnt_include_values_after_range(self):
+    requests.post(Item(0).url, "0")
+    requests.post(Item(1).url, "1")
+    requests.post(Item(2).url, "2")
+
+    read = requests.get(Range(0, 1).url)
+
+    self.assertReturns(read, [0, 1])
+
+  def test_doesnt_include_values_before_range(self):
+    requests.post(Item(0).url, "0")
+    requests.post(Item(1).url, "1")
+    requests.post(Item(2).url, "2")
+
+    read = requests.get(Range(1, 2).url)
+
+    self.assertReturns(read, [1, 2])
 
   def test_skips_missing_values(self):
     requests.post(Item(0).url, "0")
@@ -38,5 +57,11 @@ class RangeTests(DbTestCase):
 
   def test_handles_empty_ranges(self):
     read = requests.get(Range(0, 2).url)
+
+    self.assertReturns(read, [])
+
+  @timeout(1)
+  def test_with_large_empty_range(self):
+    read = requests.get(Range(0, 100000000).url)
 
     self.assertReturns(read, [])
