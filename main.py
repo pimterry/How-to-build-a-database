38c69dfe79745de51a1a9f1af9f4ef92f504a007
pimterry <pimterry@gmail.com>
2015-01-10 21:46:23 +0000

    Refactor out the data model

--- main.py
+++ main.py
@@ -2,40 +2,54 @@ import os, json, logging, cherrypy
 from blist import sorteddict
 from flask import Flask, abort, request, make_response
 
+class Database:
+    def __init__(self):
+        self.data = sorteddict()
+        self.keys = self.data.keys()
+        self.values = self.data.values()
+
+    def put(self, key, value):
+        self.data[key] = value
+
+    def get(self, key):
+        return self.data[key]
+
+    def get_range(self, start_key, end_key):
+        start_index = self.keys.bisect_left(start_key)
+        end_index = self.keys.bisect_right(end_key)
+
+        return self.values[start_index:end_index]
+
+    def clear(self):
+        self.data.clear()
+
 def build_app():
   app = Flask(__name__)
   app.debug = True
 
-  data = sorteddict()
-  keys = data.keys()
-  values = data.values()
+  database = Database()
 
   @app.route("/reset", methods=["POST"])
   def reset():
-      data.clear()
+      database.clear()
       return make_response("", 200)
 
   @app.route("/<int:item_id>", methods=["GET"])
   def get_item(item_id):
     try:
-      return "%s" % (data[item_id],)
+      return "%s" % (database.get(item_id),)
     except KeyError:
       raise abort(404)
 
   @app.route("/range")
   def get_range():
     start, end = int(request.args.get('start')), int(request.args.get('end'))
-
-    start_index, end_index = keys.bisect_left(start), keys.bisect_right(end)
-
-    relevant_values = values[start_index:end_index]
-
-    return json.dumps(relevant_values)
+    return json.dumps(database.get_range(start, end))
 
   @app.route("/<int:item_id>", methods=["POST"])
   def post_item(item_id):
     value = int(request.data)
-    data[item_id] = value
+    database.put(item_id, value)
     return make_response(str(value), 201)
 
   return app
