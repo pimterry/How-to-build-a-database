d14f342f441fa57c2c0379d283e7bbca781f8f82
Tim Perry <tim.perry@softwire.com>
2015-01-23 21:35:52 +0000

    Add secondary indexes and allow queries

--- docs-test.py
+++ docs-test.py
@@ -1,6 +1,6 @@
 from dbtestcase import DbTestCase
 from timeout_decorator import timeout
-import requests, json
+import requests, json, time
 
 class DocumentTest(DbTestCase):
     def test_can_write_and_read_document(self):
@@ -10,3 +10,27 @@ class DocumentTest(DbTestCase):
         read = requests.get("http://localhost:8080/1")
 
         self.assertReturns(read, value)
+
+    def test_can_query_by_field(self):
+        clareDoc = { "name": "Clare", "population": 655 }
+        pembrokeDoc = { "name": "Pembroke", "population": 597 }
+
+        requests.post("http://localhost:8080/1", json.dumps(clareDoc))
+        requests.post("http://localhost:8080/2", json.dumps(pembrokeDoc))
+
+        read = requests.get("http://localhost:8080/by/name/Clare")
+
+        self.assertReturns(read, clareDoc)
+
+    def test_can_quickly_query_by_field(self):
+        data = { i: { "name": "Employee %s" % i, "age": 100 }
+                 for i in range(0, 10000)}
+        requests.post("http://localhost:8080", json.dumps(data))
+
+        start = time.time()
+        read = requests.get("http://localhost:8080/by/name/Employee 9999")
+        query_millis = time.time() - start
+
+        self.assertReturns(read, { "name": "Employee 9999", "age": 100 })
+        self.assertLess(query_millis, 0.01)
+
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from flask import Flask, abort, request, make_response
 
 
 class Database:
-    def __init__(self):
+    def __init__(self, fields_to_index):
         self.data = blist.sorteddict()
+        self.indexes = { index: blist.sorteddict() for index in fields_to_index }
 
     def get_item(self, key):
         return self.data[key]
@@ -12,11 +13,27 @@ class Database:
     def put_item(self, key, value):
         self.data[key] = value
 
+        for field_name in self.indexes:
+            try:
+                index = self.indexes[field_name]
+                field_value = value[field_name]
+                index[field_value] = value
+            except (KeyError, TypeError):
+                pass
+
+
     def get_range(self, start, end):
         start_index = self.data.keys().bisect_left(start)
         end_index = self.data.keys().bisect_right(end)
 
         return self.data.values()[start_index:end_index]
+
+    def get_by_field(self, field_name, field_value):
+        if field_name in self.indexes:
+            index = self.indexes[field_name]
+            return index[field_value]
+        else:
+            raise RuntimeError("Attempt to query for non-indexed field")
 
     def reset(self):
         self.data.clear()
@@ -26,7 +43,7 @@ def build_app():
     app = Flask(__name__)
     app.debug = True
 
-    database = Database()
+    database = Database(["name"])
 
     @app.route("/<int:item_id>")
     def get_item(item_id):
@@ -47,6 +64,20 @@ def build_app():
         end = int(request.args.get('end'))
 
         return json.dumps(database.get_range(start, end))
+
+    @app.route("/by/<field_name>/<field_value>")
+    def get_by_field(field_name, field_value):
+        try:
+            return json.dumps(database.get_by_field(field_name, field_value))
+        except KeyError:
+            return abort(404)
+
+    @app.route("/", methods=["POST"])
+    def batch_upload():
+        data = json.loads(request.data.decode('utf-8'))
+        for key in data:
+            database.put_item(key, data[key])
+        return make_response("ok", 201)
 
     @app.route("/reset", methods=["POST"])
     def reset():
