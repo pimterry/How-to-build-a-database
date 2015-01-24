e7cc6c7f91b4f914a5ef792c5201e30d2a6693ed
Tim Perry <tim.perry@softwire.com>
2015-01-24 14:39:03 +0000

    Key value store

--- key-value-test.py
+++ key-value-test.py
@@ -6,3 +6,10 @@ class KeyValueTest(DbTestCase):
         read = requests.get("http://localhost:8080/123")
 
         self.assertEquals(read.status_code, 404)
+
+    def test_can_read_writes(self):
+        requests.post("http://localhost:8080/1", "5")
+
+        read = requests.get("http://localhost:8080/1")
+
+        self.assertReturns(read, 5)
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-import logging, cherrypy, blist
-from flask import Flask
+import logging, cherrypy, blist, json
+from flask import Flask, request, abort
 
 
 class Database:
     def __init__(self):
-        pass
+        self.data = { }
 
+    def get_item(self, item_id):
+        return self.data[item_id]
+
+    def put_item(self, item_id, value):
+        self.data[item_id] = value
 
 def build_app():
     app = Flask(__name__)
     app.debug = True
+
+    database = Database()
+
+    @app.route("/<int:item_id>")
+    def get_item(item_id):
+        try:
+            return json.dumps(database.get_item(item_id))
+        except KeyError:
+            return abort(404)
+
+    @app.route("/<int:item_id>", methods=["POST"])
+    def put_item(item_id):
+        value = int(request.data)
+        database.put_item(item_id, value)
 
     @app.route("/reset", methods=["POST"])
     def reset():
